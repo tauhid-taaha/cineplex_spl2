9552dbedd08366b9b179839b42bf090958bc5f65
Tashfia123 <tashfiahassan@iut-dhaka.edu>
2024-04-14 04:41:56 -0700

    login

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Home from './home/home';
 import MovieList from './components/movielist/movielist';
 import Movie from './components/movieDetail/movie';
+import Login from './components/Login/Login';
+import LoginSuccessful from './components/LoginSuccessful/LoginSuccessful';
+import Signup from './components/Signup/Signup'; // Import the Signup component
+import SignUpSuccessful from './components/SignUpSuccessful/SignUpSuccessful'; // Import the SignUpSuccessful component
+
+
 
 
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Header />
- <Routes>
-
-    <Route index element={<Home></Home>}></Route>
-    <Route path="movie/:id" element={<Movie />}></Route>
-    <Route path="movies/:type" element={<MovieList />}></Route>
-    <Route path="/*" element={<h1>error </h1>}></Route>
-  </Routes>
-</Router>
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/movie/:id" element={<Movie />} />
+                    <Route path="/movies/:type" element={<MovieList />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/login-successful" element={<LoginSuccessful />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/signup-successful" element={<SignUpSuccessful />} /> 
+                    <Route path="/*" element={<h1>Error</h1>} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
